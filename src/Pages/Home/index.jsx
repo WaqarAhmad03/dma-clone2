d674872843503eb4ade026948d8d0341aaef7398
Waqar Ahmad <waqarsow461@gmail.com>
2024-12-24 10:07:39 -0500

    verifying

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../Components/Layouts/Header'
+import Footer from '../../Components/Layouts/Footer'
+import Layout from '../../Components/Layouts/Layout'
+
+const index = () => {
+  return (
+    <>
+        <Layout>
+        Home
+        </Layout>
+    </>
+  )
+}
+
+export default index
